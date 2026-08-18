8bc60b0ff16b89977b4f9730e7009aefa2ff3eed
Alex Garcia Amezcua <alexgadev@hotmail.com>
2026-08-18 20:05:49 +0200

    window handler definition header

---
+++ src/core/window.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <GLFW/glfw3.h>
+
+class WindowHandler
+{
+    public:
+        WindowHandler(const char* title, unsigned int width = 800, unsigned int height = 600);
+        ~WindowHandler();
+
+        WindowHandler(const WindowHandler&) = delete;
+        WindowHandler& operator=(const WindowHandler&) = delete;
+        WindowHandler(WindowHandler&& other) noexcept;
+        WindowHandler& operator=(WindowHandler&& other) noexcept;
+        
+        bool isValid() const;
+
+        GLFWwindow* getHandle() const;
+
+        bool shouldClose() const; 
+        void swapBuffers();
+        void pollEvents();
+        void close();
+
+    private:
+        unsigned int m_scr_width;
+        unsigned int m_scr_height;
+
+        GLFWwindow* m_window;
+};
